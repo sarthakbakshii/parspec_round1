2dc6bb132461635e42ab8720c475e070c8429977
sarthakbakshii <sarthakbakshi14@gmail.com>
2023-05-08 13:44:14 +0530

    added parse check in fetch request

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -1,5 +1,10 @@
 export const getApiResponse = async (api) => {
-  const response = await fetch(api);
+  const response = await fetch(api,{
+  headers: {
+    "Content-Type": "application/json",
+    "Custom-Header": "custom-value",
+  },
+});
   const data = await response.json();
   return data;
 };
